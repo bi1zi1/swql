badbecd9ffa6e861083e2614d9cde5196b41ba77
Aleksandar Mihailovski <a.mihailovski@gmail.com>
2018-10-07 21:27:28 +0200

    Configure Project
    To begin with, let's create a default single view app project with support for iOS 12 and above.
    Configure the app to run only for iPhones in portrait only and set the initial app icon.
    Add CocoaPods support for 3rd party dependency management.
    Add our first pod Apollo iOS which is a strongly-typed, caching GraphQL client for iOS, written in Swift.

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'swql' do
+    # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+    use_frameworks!
+
+    # Pods for swql
+    pod 'Apollo', '~> 0.9'
+
+    target 'swqlTests' do
+    inherit! :search_paths
+    # Pods for testing
+    end
+end
---
+++ swql/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swql
+//
+//  Created by Aleksandar Mihailovski on 2018-10-07.
+//  Copyright © 2018 Aleksandar Mihailovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
